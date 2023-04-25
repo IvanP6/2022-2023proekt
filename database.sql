30bc55ff3674a0c5e7218ace15cb28cc906b098f
Ivan Panov <ivanpanovtr@abv.bg>
2023-04-25 15:21:06 +0300

    vasassfdd

---
+++ database.sql
@@ -0,0 +1,7 @@
+DROP DATABASE  IF EXISTS aurora
+CREATE DATABASE aurora;
+USE aurora;
+create table potrebiteli(id INT, name VARCHAR(255),
+pass VARCHAR(255), mail VARCHAR(255));
+INSERT INTO potrebiteli VALUES
+(1, "admin", "admin", "admin@local.bg"): 
